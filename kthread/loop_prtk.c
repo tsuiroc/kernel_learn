33b7b70b5010ceb945b7ec015d3acda4f9cd14f5
崔氏小农 <chinese_cp@126.com>
2013-10-08 17:00:10 +0800

    使用kthread_run函数创建内核函数，模块使用正常。打印cuipeng

---
+++ kthread/loop_prtk.c
@@ -0,0 +1,58 @@
+#include <linux/kthread.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/sched.h>
+
+#include <asm/signal.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("崔氏小农");
+
+struct task_struct *loop_printk_fun = NULL;
+
+
+static int loop_printk(void *data)
+{
+    allow_signal(SIGKILL);
+
+    char *ch = kmalloc(strlen((char *)data) + 1, GFP_KERNEL);
+
+    memset(ch, 0x00, sizeof(ch));
+    memcpy(ch, data, strlen(data));
+
+    while(1)
+    {
+        if(kthread_should_stop()) break;
+        printk("\n%s",ch);
+
+        set_current_state(TASK_INTERRUPTIBLE);
+                //TASK_UNINTERRUPTIBLE 不可以中断，TASK_INTERRUPTIBLE 可以
+        schedule_timeout(5 * HZ);
+    }
+
+    return 0;
+}
+
+static int __init init_kthread(void)
+{
+    printk("\nmodule init");
+
+    loop_printk_fun = kthread_run(loop_printk, "cuipeng", "loop_prtk");
+    if(NULL == loop_printk_fun) return -1;
+
+    return 0;
+}
+
+
+static void __exit exit_kthread(void)
+{
+    printk("\nmodule exit");
+    kthread_stop(loop_printk_fun);
+
+    return;
+
+}
+
+module_init(init_kthread);
+module_exit(exit_kthread);
